3d9c2529168d35dadeb55be78b0be42dc2f474f2
Peter Halliday <pghalliday@gmail.com>
2012-10-02 14:21:45 +0200

    have made the mocha task an npm module

--- grunt.js
+++ grunt.js
@@ -1,113 +1,37 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Add our custom tasks.
-  grunt.loadTasks('src/grunt');
+  // Add mochaTest task
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   // Project configuration.
   grunt.initConfig({
     lint: {
-      grunt: ['grunt.js', 'src/grunt/**/*.js'],
-      gruntTest: ['test/grunt/**/*.js'],
-      server: ['src/server/**/*.js'],
-      serverTest: ['test/server/**/*.js'],
-      client: ['src/client/**/*.js', 'test/client/**/*.js']
+      files: ['grunt.js', 'src/**/*.js']
     },
-    mocha: {
-      grunt: ['test/grunt/**/*.test.js'],
-      server: ['test/server/**/*.test.js']
+    mochaTest: {
+      files: ['test/**/*.test.js']
     },
     jshint: {
-      grunt: {
-        options: {
-          curly: true,
-          eqeqeq: true,
-          immed: true,
-          latedef: true,
-          newcap: true,
-          noarg: true,
-          sub: true,
-          undef: true,
-          boss: true,
-          eqnull: true,
-          node: true
-        },
-        globals: {
-        }
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true
       },
-      gruntTest: {
-        options: {
-          curly: true,
-          eqeqeq: true,
-          immed: true,
-          latedef: true,
-          newcap: true,
-          noarg: true,
-          sub: true,
-          undef: true,
-          boss: true,
-          eqnull: true,
-          node: true
-        },
-        globals: {
-          describe: false,
-          it: false
-        }
-      },
-      server: {
-        options: {
-          curly: true,
-          eqeqeq: true,
-          immed: true,
-          latedef: true,
-          newcap: true,
-          noarg: true,
-          sub: true,
-          undef: true,
-          boss: true,
-          eqnull: true,
-          node: true
-        },
-        globals: {
-        }
-      },
-      serverTest: {
-        options: {
-          curly: true,
-          eqeqeq: true,
-          immed: true,
-          latedef: true,
-          newcap: true,
-          noarg: true,
-          sub: true,
-          undef: true,
-          boss: true,
-          eqnull: true,
-          node: true
-        },
-        globals: {
-          describe: false,
-          it: false
-        }
-      },
-      client: {
-        options: {
-          curly: true,
-          eqeqeq: true,
-          immed: true,
-          latedef: true,
-          newcap: true,
-          noarg: true,
-          sub: true,
-          undef: true,
-          boss: true,
-          eqnull: true
-        },
-        globals: {
-        }
+      globals: {
+        describe: false,
+        it: false
       }
     },
-    mochaConfig: {
+    mochaTestConfig: {
       options: {
         reporter: 'nyan'        
       }
@@ -115,5 +39,5 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint mocha');
+  grunt.registerTask('default', 'lint mochaTest');
 };
