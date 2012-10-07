96c9a7725c1f19785efd0ebd93373e65a14885df
Peter Halliday <pghalliday@gmail.com>
2012-10-07 18:38:38 +0200

    should use a different port for listener tests now that the client continuously reconnects

--- test/Listener/Listener.test.js
+++ test/Listener/Listener.test.js
@@ -7,14 +7,17 @@ describe('Listener', function() {
   var server;
   
   before(function(done) {
-    server = new Server(8080);
+    // NB. use a different socket to other tests as otherwise
+    // any reconnecting browser instances may interfere with those other tests
+    server = new Server(8081);
     server.start(done);
   });
 
   it('should connect on load and reconnect if the server bounces', function(done) {
-    // extend the mocha test timeout as we will wait for someone to connect using a browser
+    // extend the mocha test timeout as we will wait for someone to connect
+    // using a browser if not already loaded
     this.timeout(10000);
-    console.log('You have 10 seconds to load http://localhost:8080 in a browser...');
+    console.log('You have 10 seconds to load http://localhost:8081 in a browser...');
     server.once('listener', function() {
       server.stop(function() {
         server.start(function() {
